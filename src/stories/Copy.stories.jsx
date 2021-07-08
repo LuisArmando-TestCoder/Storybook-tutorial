51ba33a05d74eea73e5da86c598735264c3795f4
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-07-08 17:16:24 -0600

    add own component

---
+++ src/stories/Copy.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// add a loader for your ts library with webpack
+// import * as Components from '../components'
+
+const Random = () => (
+  <h1>This is random</h1>
+)
+
+export default {
+  title: 'Components/Custom/Random',
+  component: Random
+}
